546d4a1a401067e398396f8337f7a44ab46b22f1
saurabhbansal2443 <saurabhbansal2443@gmail.com>
2024-07-06 12:46:11 +0530

    netflix Logo

---
+++ src/Components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import netflixLogo from "../assets/netflixlogo.png"
+
+const Header = () => {
+  return (
+    <div className='w-full '>
+        <img src={netflixLogo}></img>
+      
+    </div>
+  )
+}
+
+export default Header
--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import Header from './Header'
 
 const Login = () => {
   return (
     <div>
+        <Header></Header>
       Login Page 
     </div>
   )
-}
+}  
 
 export default Login
